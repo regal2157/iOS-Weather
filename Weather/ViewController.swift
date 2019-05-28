dd16de30d39edf3d995ce1cb99d5d9b59818dcfd
John O'Connell <regal2157@yahoo.com>
2019-05-28 15:00:21 -0400

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by John O'Connell on 5/28/19.
+//  Copyright © 2019 John O'Connell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
